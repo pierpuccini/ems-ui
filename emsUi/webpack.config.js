a18b6b76c79a8a88197b93c679845d0a0a5ec72f
pierpuccini <pierpuccini@gmail.com>
2020-10-03 15:35:49 -0500

    Created mango module

---
+++ emsUi/webpack.config.js
@@ -0,0 +1,7 @@
+
+/*
+ * Copyright (C) 2020 Infinite Automation Systems Inc. All rights reserved.
+ */
+
+const moduleConfig = require('@infinite-automation/mango-module-tools');
+module.exports = moduleConfig();
