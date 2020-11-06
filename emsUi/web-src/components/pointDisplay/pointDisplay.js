fef04da9f989c7f8b0bae15102e0b261c05aee4c
pierpuccini <pierpuccini@gmail.com>
2020-11-05 19:20:28 -0500

    Completed point set

--- emsUi/web-src/components/pointDisplay/pointDisplay.js
+++ emsUi/web-src/components/pointDisplay/pointDisplay.js
@@ -12,7 +12,11 @@ class PointDisplayController {
     }
 
     static get $inject() {
-        return [];
+        return ['maDialogHelper'];
+    }
+
+    constructor(maDialogHelper) {
+        this.maDialogHelper = maDialogHelper;
     }
 
     $onChanges(changes) {
@@ -20,6 +24,37 @@ class PointDisplayController {
             this.cost = this.points.find((point) => point.name === 'Cost Ranges');
             this.load = this.points.find((point) => point.name === 'Nominal Active Power');
         }
+    }
+
+    calculateNewLoad(operation, percent) {
+        if (operation === '+') {
+            this.newLoad = this.load.value * (1 + percent / 100);
+        } else {
+            this.newLoad = this.load.value * (1 - percent / 100);
+        }
+        this.newLoadWithSuffix = this.newLoad.toFixed(2) + this.load.textRenderer.suffix;
+    }
+
+    setNewLoad(event) {
+        const toMsg = [this.load.renderedValue, this.newLoadWithSuffix].join(' -> ');
+        const loadValueCopy = angular.copy(this.load.value);
+        this.maDialogHelper.confirm(event, ['ems.demand.confirmChange', toMsg]).then(
+            (res) => {
+                if (!this.maDialogHelper.isCancelled(res)) {
+                    console.log('revert in res');
+                    this.load.value = loadValueCopy;
+                } else {
+                    console.log('set');
+                    this.load.setValue(this.newLoad);
+                }
+            },
+            (error) => {
+                if (!this.maDialogHelper.isCancelled(error)) {
+                    this.load.value = loadValueCopy;
+                    console.log('revert');
+                }
+            }
+        );
     }
 }
 
